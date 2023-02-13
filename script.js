6d10e0b938c869cb9501a1ce6dfbe7803e664cd5
Jack Larson <91426054+Jackal-42@users.noreply.github.com>
2023-02-12 20:54:58 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+function toDesmos(str){
+    terms = str.split(" + ");
+    for(let i = 0; i < terms.length; i++){
+        if(terms[i].includes("x^")){
+            let powerSplit = terms[i].split("x^");
+            terms[i] = powerSplit[0] + "x^{" + powerSplit[1] + "}";
+        }
+        if(terms[i].includes("E")){
+            let eSplit = terms[i].split("E");
+            terms[i] = "\\left("+ eSplit[0] +"\\cdot10^{" + eSplit[1].split("x")[0] + "}\\right)x" + eSplit[1].split("x")[1];
+        }
+    }
+    document.getElementById("desmos").value = terms.join("+");
+}
